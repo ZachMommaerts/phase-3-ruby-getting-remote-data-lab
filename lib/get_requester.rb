0e16900779d81d88c7bfaf63bb58ad8301edfa22
Zach <zmommaerts@gmail.com>
2022-01-05 12:01:22 -0600

    stuck

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,17 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    attr_accessor :url
+
+    def initialize (string)
+        @url = string
+    end
+
+    def get_response_body(url)
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+end
