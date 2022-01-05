b4153ab55ba76c4784381afca7995f02f136ce07
Zach <zmommaerts@gmail.com>
2022-01-05 12:05:33 -0600

    commit tree a195a3f895058dab65f0d0ceba45b7a3c1952e74

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,15 +3,18 @@ require 'open-uri'
 require 'json'
 
 class GetRequester
-    attr_accessor :url
 
-    def initialize (string)
-        @url = string
+    def initialize (url)
+        @url = url
     end
 
-    def get_response_body(url)
-        uri = URI.parse(url)
+    def get_response_body
+        uri = URI.parse(@url)
         response = Net::HTTP.get_response(uri)
         response.body
+    end
+
+    def parse_json
+        body = JSON.parse(self.get_response_body)
     end
 end
